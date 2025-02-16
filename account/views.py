398e08eef09572290e3d3326880664ec05d7674e
rasouli <z.rasouli.18a@gmail.com>
2025-02-16 16:43:54 +0330

    page login and logout and authontication is work

---
+++ account/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login, logout
+
+def user_login(request):
+    if request.user.is_authenticated:
+        return redirect('/')
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+    return render(request,'account/login.html', context={})
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('/')
