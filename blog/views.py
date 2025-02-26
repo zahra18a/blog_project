3590b627d3cca9c4bf440b5af5ce0866b3cdaec2
zahra18a <zahra.rasouli.18a@gmail.com>
2025-02-26 11:58:55 +0330

    add create detail page and create object template base.html

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from blog.models import Article
 
-# Create your views here.
+
+def article_detail(request, pk):
+    # article = Article.objects.get(id=pk)
+    article = get_object_or_404(Article,id=pk)
+    return render(request, 'blog/article-details.html', context={'article': article})
