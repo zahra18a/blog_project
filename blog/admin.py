1194a4e8b5985672c54d8c37b4374b85530585be
zahra18a <zahra.rasouli.18a@gmail.com>
2025-03-09 15:57:33 +0330

    change admin page

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,35 @@
 from django.contrib import admin
+from django.utils.text import Truncator
+
 from . import models
 
-admin.site.register(models.Article)
+class FilterByTitle(admin.SimpleListFilter):
+    title = 'بر اساس کلید های پر تکرار'
+    parameter_name = 'title'
+    def lookups(self, request, model_admin):
+        return (
+            ('django', 'جنگو'),
+            ('python','پایتون')
+        )
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(title__icontains=self.value())
+
+@admin.register(models.Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'short_body', 'author', 'show_image')
+    list_display_links = ('short_body',)
+    list_editable = ('title',)
+    list_filter = ('pub_date','created',FilterByTitle)
+    search_fields = ('title','body')
+    fields = ('image','title', 'body')
+
+    def short_body(self, obj):
+        return Truncator(obj.body).chars(50)
+
+    short_body.short_description = "خلاصه متن"
+
+# admin.site.register(models.Article)
 admin.site.register(models.Category)
 admin.site.register(models.Comment)
 admin.site.register(models.Message)
